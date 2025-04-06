fa78853f1b3121208e7f6d02460f17041cfebbc7
Katie <bewimsical@gmail.com>
2025-04-06 17:11:23 -0400

    correctly configured lombok. Created a group model

---
+++ src/main/java/edu/farmingdale/getgame/model/Group.java
@@ -0,0 +1,23 @@
+package edu.farmingdale.getgame.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "group")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Group {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long groupID;
+    private String name;
+    private String date;
+    private String location;
+
+}
