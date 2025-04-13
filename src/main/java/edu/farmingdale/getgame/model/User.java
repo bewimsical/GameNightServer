d10238f44b073c97db8f98628c7043532cead402
Katie <bewimsical@gmail.com>
2025-04-12 21:31:45 -0400

    added friends set to user class. Set up many to many relationship

--- src/main/java/edu/farmingdale/getgame/model/User.java
+++ src/main/java/edu/farmingdale/getgame/model/User.java
@@ -24,6 +24,14 @@ public class User{
     )
     private Set<Game> games;
 
+    @ManyToMany
+    @JoinTable(
+            name = "user_user",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "friend_id")
+    )
+    private Set<User> friends;
+
     public User() {
     }
 
@@ -90,6 +98,13 @@ public class User{
 
     public void setGames(Set<Game> games) {
         this.games = games;
+    }
+
+    public Set<User> getFriends() {
+        return friends;
+    }
+    public void setFriends(Set<User> friends) {
+        this.friends = friends;
     }
 
     @Override
