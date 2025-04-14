2b0f22b55e6399d3e8a2bc5143bb1b2fbee429f7
Kathleen Wims <bewimsical@gmail.com>
2025-04-14 19:13:22 -0400

    added get mappings for user games, friends and parties

--- src/main/java/edu/farmingdale/getgame/controller/UserController.java
+++ src/main/java/edu/farmingdale/getgame/controller/UserController.java
@@ -2,13 +2,17 @@ package edu.farmingdale.getgame.controller;
 
 import edu.farmingdale.getgame.dto.UserDto;
 import edu.farmingdale.getgame.exception.ResourceNotFoundException;
+import edu.farmingdale.getgame.model.Game;
+import edu.farmingdale.getgame.model.Party;
 import edu.farmingdale.getgame.model.User;
 import edu.farmingdale.getgame.service.GameService;
 import edu.farmingdale.getgame.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 @RestController
 @RequestMapping(path = "/users")
@@ -26,9 +30,30 @@ public class UserController {
         System.out.println(userDto.getUsername());
         return userService.createUser(userDto);
     }
+    @GetMapping("/{id}")
+    public User getUser(@PathVariable String id){
+        long userId = Long.parseLong(id);
+        Optional<User> user = userService.getUser(userId);
+        return user.orElseThrow(() -> new ResourceNotFoundException("User not found with id: " + id));
+    }
+    @GetMapping("/{id}/games")
+    public Set<Game> getGames(@PathVariable Long id){
+        return userService.getUserGames(id);
+    }
+    @GetMapping("/{id}/friends")
+    public Set<User> getFriends(@PathVariable Long id){
+        return userService.getUserFriends(id);
+    }
+    @GetMapping("/{id}/parties")
+    public List<Party> getParties(@PathVariable Long id){
+        return userService.getUserParties(id);
+    }
+    @GetMapping("/{id}/hosted")
+    public List<Party> getHostedParties(@PathVariable Long id){
+        return userService.getHostedParties(id);
+    }
     @PostMapping("/addgame")
     public void addGame(@RequestParam Long user, @RequestParam int game){
-
         userService.addGame(user,game);
     }
     @DeleteMapping("deletegame")
@@ -37,7 +62,6 @@ public class UserController {
     }
     @PostMapping("/addfriend")
     public void addFriend(@RequestParam Long user, @RequestParam Long friend){
-
         userService.addFriend(user,friend);
     }
     @DeleteMapping("deletefriend")
@@ -45,12 +69,7 @@ public class UserController {
         userService.removeFriend(user, friend);
     }
 
-    @GetMapping("/{id}")
-    public User getUser(@PathVariable String id){
-        long userId = Long.parseLong(id);
-        Optional<User> user = userService.getUser(userId);
-        return user.orElseThrow(() -> new ResourceNotFoundException("User not found with id: " + id));
-    }
+
 
 
 }
